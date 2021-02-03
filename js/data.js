e37250c9592647f23c71fbadbfe9c6cac2a42c81
dylan <dylansalim3@hotmail.com>
2021-02-03 16:02:34 +0800

    Edit Animation

---
+++ js/data.js
@@ -0,0 +1,62 @@
+var portfolioData = [
+    {
+        "title": "QR Menu Application",
+        "category": "Web Application",
+        "date": "18/08/2020",
+        "description": ` <p>A mobile application developed using Java and Node JS, deployed in a self-hosted
+        VPS with Docker.
+        <br>
+        Users can use this app to create a custom menu, QR code that
+        represents the menu will be generated to be scanned and view the menu. <br><br>
+        Purpose of Project
+    <ul>
+        <li>To create a mobile application that enables users to customize and generate
+            catalog menus digitally
+        </li>
+        <li>To enable the ease of accessing digitalized menu through QR code</li>
+    </ul>
+    </p>`,
+        "url": "https://github.com/dylansalim3/WIX3004_QR_Menu_App",
+        "img":"img/qrmenu.png"
+    },
+    {
+        "title": "",
+        "category": "",
+        "date": "",
+        "description": "",
+        "url": "",
+        "img":"img/qrmenu.png"
+    },
+    {
+        "title": "",
+        "category": "",
+        "date": "",
+        "description": "",
+        "url": "",
+        "img":"img/qrmenu.png"
+    },
+    {
+        "title": "",
+        "category": "",
+        "date": "",
+        "description": "",
+        "url": "",
+        "img":"img/qrmenu.png"
+    },
+    {
+        "title": "",
+        "category": "",
+        "date": "",
+        "description": "",
+        "url": "",
+        "img":"img/qrmenu.png"
+    },
+    {
+        "title": "",
+        "category": "",
+        "date": "",
+        "description": "",
+        "url": "",
+        "img":"img/qrmenu.png"
+    },
+]
